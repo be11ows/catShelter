d7622038c9e326a3fa3c959a50ff12b983861ef1
istvan kosa <istvankosa@istvans-iMac.home>
2021-07-26 23:53:03 -0400

    Implemented 'database' and access all views

---
+++ handlers/home.js
@@ -0,0 +1,42 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const cats = require('../data/cats');
+
+
+
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname; 
+    console.log(pathname);
+
+    
+    if (pathname === '/' && req.method === 'GET') {
+        // implement the logic for showing home html view
+        let filepath = path.normalize(
+            path.join(__dirname, "../views/home/index.html")
+        );
+
+        fs.readFile(filepath, (err, data) => {
+            if(err) {
+                console.log(err);
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write(404);
+                res.end();
+                return;
+            }
+
+            res.writeHead(200,{
+                'Content-Type': 'text/html'
+            });
+
+            res.write(data);
+            res.end();
+        });
+
+    } else {
+        return true;
+    }
+};
